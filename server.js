a755b28f55f769f604a6bfc96724b6fee7d0b6d6
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-14 12:34:35 -0400

    Implemented /api/fun-fact and a note at / suggesting users go to /api/fun-fact.

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+const express = require("express")
+const axios = require("axios")
 
+const app = express()
+
+const PORT = 3000
+const ENDPOINT = "https://uselessfacts.jsph.pl/api/v2/facts/random"
+
+app.get("/", (req, res) => {
+  res.send(
+    "<p>Why don't you try getting a <a href='/api/fun-fact'>fun fact?</a></p>"
+  )
+})
+
+app.get("/api/fun-fact", async (req, res) => {
+  try {
+    const response = await axios.get(ENDPOINT)
+    res.json({
+      fact: response.data.text,
+    })
+  } catch (error) {
+    if (error.response) {
+      console.error("API Error:", error.response.status, error.response.data)
+    } else {
+      console.error("Network Error:", error.message)
+      res.status(500).json({ error: "Could not fetch fun fact." })
+    }
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
